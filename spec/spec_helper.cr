5a2f641d655f87cef276b5ce04d6c4c1b8bca8ef
maiha <maiha@wota.jp>
2019-12-17 16:38:00 +0900

    spec: remove reprecated Markdown module for crystal-0.32.0

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,17 +2,22 @@ require "spec"
 require "spec2"
 
 require "../src/protobuf-storage"
-require "./markdown"
 
 include Spec2::GlobalDSL
 
 class Readme
-  def initialize(path : String)
-    @renderer = Crystal::Doc::Markdown::MemoryRenderer.new
-    Crystal::Doc::Markdown.parse(File.read(path), @renderer)
+  def initialize(@path : String)
+    @buf = File.read(@path)
   end
 
-  delegate code, to: @renderer
+  def code(label) : String
+    head, body = @buf.split(/^#{label}/m,2)
+    body || raise "not found '#{label}' in #{@path}"
+    body.scan(/^```.*?\n(.*?)\n```/m) do
+      return $1
+    end
+    raise "not found code within '#{label}' in #{@path}"
+  end
 end
 
 README = Readme.new(File.join(__DIR__, "../README.md"))
